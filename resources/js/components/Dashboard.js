3126d4ec37f611052c66950a04ca5bc1394da24b
kleber.santos <kleber.santos@sanp.com.br>
2021-09-06 15:21:34 -0300

    initial files

---
+++ resources/js/components/Dashboard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Dashboard() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Dashboard</div>
+
+                        <div className="card-body">Aqui estamos usando Laravel8 + React</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
+
+if (document.getElementById('dashboard')) {
+    ReactDOM.render(<Dashboard />, document.getElementById('dashboard'));
+}
